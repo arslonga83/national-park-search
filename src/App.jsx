b3e57148d443e6fe68c43e28adddcfe77389ad06
arslonga83 <jlwood83@gmail.com>
2023-01-22 14:11:21 -0800

    map api data to create cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 
 import './App.css'
-import { data } from './data.js'
+// import { data } from './data.js'
 import Card from './Components/Card'
 import Header from './Components/Header'
 
 const API_KEY = import.meta.env.VITE_API_KEY
-console.log(API_KEY)
 
-// fetch(`https://developer.nps.gov/api/v1/parks?stateCode=OR&api_key=${API_KEY}`)
-//   .then(res => res.json())
-//   .then(data => console.log(data.data[1]))
+const parksData = await fetch(`https://developer.nps.gov/api/v1/parks?stateCode=OR&api_key=${API_KEY}`)
+  .then(res => res.json())
+  .then(data => {
+    return data.data
+})
 
-// national park service, parks search by state
-// delivers a nested object - all this stuff is in 'data'
-//fullName, url, description, latitude, longitude, addresses[(city)], images[(title, url, credit, altText, caption)]
-
-console.log(data.data[0].fullName)
+const cards = parksData.map(park => {
+  return (
+    <Card 
+      key={park.id}
+          park={park}
+    />
+  )
+})
 
 function App() {
 
@@ -23,12 +27,8 @@ function App() {
     <div className="App">
       <Header />
       <main>
-        <Card data={data}/>
-        <Card data={data}/>
-        <Card data={data}/>
-        <Card data={data}/>
-      </main>
-      
+        {cards}
+      </main> 
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,12 @@
 
 export default function Card(props) {
+  
   return (
     <div className="card">
-      <h1>{props.data.data[0].fullName}</h1>
-      <p><a href={props.data.data[0].url}>{props.data.data[0].url}</a></p>
-      <p>{props.data.data[0].description}</p>
-      <img src={props.data.data[0].images[0].url} />
+      <h1>{props.park.fullName}</h1>
+      <p><a href={props.park.url}>{props.park.url}</a></p>
+      <p>{props.park.description}</p>
+      <img src={props.park.images[0].url} />
     </div>
    
   )
