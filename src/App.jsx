f50872a2029eb6c499cd43b2e7b3b9c411cad9e3
arslonga83 <jlwood83@gmail.com>
2023-01-25 14:56:32 -0800

    start refactoring code with useEffect so i can connect form input

--- src/App.jsx
+++ src/App.jsx
@@ -9,44 +9,63 @@ const API_KEY = import.meta.env.VITE_API_KEY
 
 
 
-const cardsData = await fetch(`https://developer.nps.gov/api/v1/parks?stateCode=OR&api_key=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => {
-    return data.data.map(park => {
-      return (
-        <Card 
-          key={park.id}
-          park={park}
-        />
-      )
-    })
-})
+// const cardsData = await fetch(`https://developer.nps.gov/api/v1/parks?stateCode=OR&api_key=${API_KEY}`)
+//   .then(res => res.json())
+//   .then(data => {
+//     return data.data.map(park => {
+//       return (
+//         <Card 
+//           key={park.id}
+//           park={park}
+//         />
+//       )
+//     })
+// })
 
 // const cards = parksData
 
-
-
-
 function App() {
 
-  const [stateCode, setStateCode] = React.useState('')
-  const [cards, setCards] = React.useState(cardsData)
+  const [stateCode, setStateCode] = React.useState('OR')
+  const [parksData, setParksData] = React.useState(false)
 
-  function handleChange(event) {
-    setStateCode(event.target.value)
-  }
+  React.useEffect(() => {
+    fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${stateCode}&api_key=${API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        setParksData(data)
+      })
+  }, [])
 
-  function handleClick() {
-    console.log('click')
-  }
+  console.log(parksData)
+
+  let cards 
+  if (parksData) {
+    cards = parksData.data.map(park => {
+          return (
+            <Card 
+              key={park.id}
+              park={park}
+            />
+          )
+        })
+      }
+
+  // function handleChange(event) {
+  //   setStateCode(event.target.value)
+  // }
+
+  // function handleClick() {
+  //   console.log('click')
+  // }
 
   return (
     <div className="App">
-      <Header 
+      {/* <Header 
         stateCode={stateCode}
         handleChange={handleChange} 
         handleClick={handleClick}
-      />
+      /> */}
       
       <main>
         {cards}
